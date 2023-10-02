caefa6ad24bbd0118aae58b0c7540dc4b6782c05
Allan Juma <allancjuma@gmail.com>
2023-10-03 01:48:46 +0300

    10. fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,25 @@
 #include "lists.h"
 
+/**
+ * check_cycle - check if a singly linked list has a cycle in it
+ * @list: list to be checked
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
-    if (!list)
-        return 0;
+	listint_t *hare = list;
+	listint_t *tortoise = list;
 
-    listint_t *tortoise, *hare;
-
-    tortoise = list;
-    hare = list;
-
-    while (hare && hare->next)
-    {
-        tortoise = tortoise->next;          // Move tortoise one step
-        hare = hare->next->next;            // Move hare two steps
-
-        if (tortoise == hare)               // If they meet, then there's a cycle
-            return 1;
-    }
-
-    return 0;                               // If loop ends, no cycle detected
+	if (list == NULL)
+		return (0);
+	while (tortoise && hare)
+	{
+		if (hare->next == NULL)
+			return (0);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+			return (1);
+	}
+	return (0);
 }
