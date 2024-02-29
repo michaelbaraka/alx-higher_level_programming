89203e2a0e2c4b7678f671612d588d4f4c79eef8
Allan Juma <allancjuma@gmail.com>
2024-02-29 17:02:48 +0300

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-# Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl "$1" -sX POST -H "Content-Type: application/json" -d @"$2"
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <URL> <JSON_File>"
+    exit 1
+fi
+
+url=$1
+json_file=$2
+
+# Check if the JSON file exists
+if [ ! -f "$json_file" ]; then
+    echo "Error: JSON file not found: $json_file"
+    exit 1
+fi
+
+# Make a POST request with the content of the JSON file in the body
+curl -sX POST -d "@$json_file" -H "Content-Type: application/json" "$url"
