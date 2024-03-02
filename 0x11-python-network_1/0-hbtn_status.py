a58545d0995d7187f8dc19a830114b19ca250dbe
Allan Juma <allancjuma@gmail.com>
2024-03-02 14:23:42 +0300

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using the urllib package
+"""fetches https://alx-intranet.hbtn.io/status using the urllib package
 """
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     data = response.read()
     print("Body response:")
     print("	- type:", type(data))
